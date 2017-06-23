2a80b4978eccaf398c03732a3f938103475a5383
drg <dguyer57@gmail.com>
2017-06-23 09:21:27 -0400

    Added more functionality

--- linkwerks.py
+++ linkwerks.py
@@ -16,34 +16,56 @@ Response headers:
 
 
 def praw_reddit_authorized():
-    authorized_connection = praw.Reddit(client_id = 'yXTwupFSkGggig',
+    authorized_instance = praw.Reddit(client_id = 'yXTwupFSkGggig',
                                         client_secret = '4QsXNLGCQ1TLo2cbgJ3akyvKYBw',
                                         password = 'OmputerClaw65',
                                         user_agent = 'pythonscript:linkwho.py:v1.0 (by /u/drg_analytics)',
                                         username = 'drg_analytics' )
-    return authorized_connection
+    return authorized_instance
 
 def praw_reddit_read_only():
-    read_only_connection = praw.Reddit(client_id = 'yXTwupFSkGggig',
+    read_only_instance = praw.Reddit(client_id = 'yXTwupFSkGggig',
                                        client_secret = '4QsXNLGCQ1TLo2cbgJ3akyvKYBw',
                                        user_agent = 'pythonscript:linkwho.py:v1.0 (by /u/drg_analytics)' )
-    return read_only_connection
+    return read_only_instance
 
 def get_hot_ten(connection, subreddit_name):
-    """returns the top 10 hotest threads for a given subreddit.
+    """prints the top 10 hotest threads for a given subreddit.
 
     Args:
-        connection = praw connection (authorized or read only) to reddit
+        connection = praw Reddit instance (authorized or read only) to reddit
         subreddit_name = the desired subreddit
 
     """
 
-    for submission in connection.subreddit(subreddit_name).top(limit=10):
+    for submission in connection.subreddit(subreddit_name).hot(limit=10):
         print(submission.title)
 
-#
-reddit_connect = praw_reddit_authorized()
-#print authorized_connection.user.me()
+def get_most_linked_url(connection, subreddit_name, time):
+    """prints the 'linked-to url' for the top 100 threads for a given subreddit all time.
 
-#read_only_connection = praw_reddit_read_only()
-get_hot_ten(reddit_connect, 'all')
+    Args:
+        connection = praw Reddit instance (authorized or read only) to reddit
+        subreddit_name = the desired subreddit
+        time = for the given time period. Options: 'day', 'month', 'year','al'
+
+    """
+
+    for submission in connection.subreddit(subreddit_name).top(time):
+        print(submission.url)
+
+def stalk_a_fool(connection, user):
+    """query a user's post history in whatever way you wish
+
+    Args:
+        connection = praw Reddit instance (authorized or read only) to reddit
+        user = the user you want to 'stalk'
+
+    """
+
+    for submission in connection.redditor(user).top(time):
+        print(submission.title)
+
+
+reddit_connect = praw_reddit_authorized()
+get_most_linked_url(reddit_connect, subreddit_name= 'politics', time= 'day')
